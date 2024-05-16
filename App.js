0e9255c36f3ad3091d9fdfdef90e7032a1dde021
Kuuku Ampah <ampah.kuuku@gmail.com>
2024-05-16 18:22:48 +0000

    username and password

--- App.js
+++ App.js
@@ -1,21 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default function App(){
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+  return(
+    <SafeAreaView style={styles.interface}>
+      <View style={styles.container}>
+        <Text style={styles.title}> Log In to JooKuu</Text>
+
+        <View> 
+
+          <View style={styles.input}>
+            <Text style={styles.label}>Email</Text>
+            <TextInput 
+            style={styles.user}
+            placeholder='example@mail.com'
+            value={form.email} 
+            onChangeText={email=> setForm({...form, email})}/>
+          </View>
+
+          <View style={styles.input}>
+            <Text style={styles.label}>Password</Text>
+            <TextInput 
+            secureTextEntry
+            style={styles.user}
+            placeholder='********'
+            value={form.password} 
+            onChangeText={password=> setForm({...form, password})}/>
+          </View>
+
+          <View style={styles.press}>
+            <TouchableOpacity>
+              <View style={styles.btn}>
+                <Text style={styles.btnText}>Log In</Text>
+              </View>
+            </TouchableOpacity>
+
+          </View>
+
+
+        </View>
+        
+        
+      </View>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor:'#f2f4f5',
+    padding: 30,
+    borderRadius: 20,
+    width:340
+    
+
+  },
+  interface: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    height: 100,
+
+
   },
-});
+  title:{
+    fontSize: 27,
+    fontWeight: '700',
+    color: '#1e1e1e',
+    marginBottom: 6,
+    marginTop:45,   
+    textAlign: 'center'
+
+  },
+  input: {
+    marginBottom: 16
+  },
+  label:{
+    fontSize: 16,
+    fontWeight:'700',
+    color: '#222',
+    marginBottom:7
+
+  },
+  user:{
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 12,
+    fontSize: 11,
+    color: '#222',
+    fontWeight: '500'
+  },
+  press:{
+    backgroundColor: '#ff0000',
+    borderRadius: 8,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10
+
+  },
+  btnText:{
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '400'
+
+  }
+
+})
